86ddc4e60bb05b4390df4c68ced934fc938dc1c8
doniu997 <doniu997@gmail.com>
2019-04-01 23:31:40 +0200

    code review fixes

--- src/main/java/pl/opensource/security/SecurityConfig.java
+++ src/main/java/pl/opensource/security/SecurityConfig.java
@@ -13,22 +13,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	PasswordEncoder passwordEncoder;
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-		.formLogin().loginPage("/login").permitAll().and()
+		.httpBasic().and()
 		.csrf().disable()
 		.authorizeRequests()
-				.antMatchers("/register", "/actuator/info", "/actuator/health").permitAll()
+				.antMatchers("/register", "/actuator/info", "/actuator/health","/login", "/advertisement/","/advertisement").permitAll()
 				.anyRequest().authenticated()
 				.and()
 		.logout()
 		.logoutUrl("/logmeout")
-			.logoutSuccessUrl("/login")
-			.permitAll();
+			.logoutSuccessUrl("/login").permitAll();
 	}
 
 	@Bean
